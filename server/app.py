bfe9ad11822c4f4c926e5acc5f784234af86d74b
OumaArera <johnouma999@gmail.com>
2024-04-11 08:32:01 +0300

    feat: all tests pass

--- server/app.py
+++ server/app.py
@@ -1,30 +1,113 @@
-from distutils.log import debug
-from flask import Flask, request, jsonify, make_response
-from flask_migrate import Migrate
-from models  import Customer, db
+import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+export const SignupForm = () => {
+  const [customers, setCustomers] = useState([{}]);
+  const [refreshPage, setRefreshPage] = useState(false);
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
 
-app = Flask(__name__)
+  useEffect(() => {
+    console.log("FETCH! ");
+    fetch("/customers")
+      .then((res) => res.json())
+      .then((data) => {
+        setCustomers(data);
+        console.log(data);
+      });
+  }, [refreshPage]);
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-migrate = Migrate(app, db)
+  const formSchema = yup.object().shape({
+    email: yup.string().email("Invalid email").required("Must enter email"),
+    name: yup.string().required("Must enter a name").max(15),
+    age: yup
+      .number()
+      .positive()
+      .integer()
+      .required("Must enter age")
+      .typeError("Please enter an Integer")
+      .max(125),
+  });
 
-db.init_app(app)
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      age: "",
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      fetch("customers", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values, null, 2),
+      }).then((res) => {
+        if (res.status == 200) {
+          setRefreshPage(!refreshPage);
+        }
+      });
+    },
+  });
 
-@app.route("/customers", methods=['GET', 'POST'])
-def customers():
-    if request.method == 'GET':
-        print(([customer.to_dict() for customer in Customer.query.all()]))
-        return make_response(jsonify([customer.to_dict() for customer in Customer.query.all()]))
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit} style={{ margin: "30px" }}>
+        <label htmlFor="email">Email Address</label>
+        <br />
+        <input
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <p style={{ color: "red" }}> {formik.errors.email}</p>
+        <label htmlFor="name">Name</label>
+        <br />
 
-    if request.method == 'POST':
-        data = request.get_json()
-        customer = Customer(name=data.get('name'), email=data.get('email'), age=data.get('age'))
-        db.session.add(customer)
-        db.session.commit()
-        return make_response(
-            jsonify(
-                {'id': customer.id, 'name': customer.name, 'email': customer.email, 'age': customer.age }))
+        <input
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <p style={{ color: "red" }}> {formik.errors.name}</p>
 
-if __name__ == "__main__":
-    app.run(port="5555", debug=True)
+        <label htmlFor="age">age</label>
+        <br />
+
+        <input
+          id="age"
+          name="age"
+          onChange={formik.handleChange}
+          value={formik.values.age}
+        />
+        <p style={{ color: "red" }}> {formik.errors.age}</p>
+        <button type="submit">Submit</button>
+      </form>
+      <table style={{ padding: "15px" }}>
+        <tbody>
+          <tr>
+            <th>name</th>
+            <th>email</th>
+            <th>age</th>
+          </tr>
+          {customers === "undefined" ? (
+            <p>Loading</p>
+          ) : (
+            customers.map((customer, i) => (
+              <>
+                <tr key={i}>
+                  <td>{customer.name}</td>
+                  <td>{customer.email}</td>
+                  <td>{customer.age}</td>
+                </tr>
+              </>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
